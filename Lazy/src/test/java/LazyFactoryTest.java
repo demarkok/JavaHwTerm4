6c26c650aaf07901343b4e10ab4cda6401e99ebb
Ilya Kaysin <demarkok@gmail.com>
2017-03-01 02:02:50 +0300

    add concurrent lazy supplier test

--- Lazy/src/test/java/LazyFactoryTest.java
+++ Lazy/src/test/java/LazyFactoryTest.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
 public class LazyFactoryTest {
+    private static final int NUMBER_OF_THREADS = 100;
+
     @Test
     public void createLazy() throws Exception {
         final Counter counter = new Counter();
@@ -18,7 +22,24 @@ public class LazyFactoryTest {
 
     @Test
     public void createSynchronizedLazy() throws Exception {
+        final Counter counter = new Counter();
+        Lazy<Integer> lazy = LazyFactory.createLazy(() -> {
+            counter.inc();
+            return 1;
+        });
 
+        ArrayList<Thread> threads = new ArrayList<>();
+        Runnable task = lazy::get;
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            Thread thread = new Thread(task);
+            thread.run();
+            threads.add(thread);
+        }
+        for (Thread thread: threads) {
+            thread.join();
+        }
+        assertEquals(new Integer(1), lazy.get());
+        assertEquals(1, counter.get());
     }
 
     @Test
