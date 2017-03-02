40c58b64f4e1d3b6ecdccedc156bf73ef1fb1fd1
Ilya Kaysin <demarkok@gmail.com>
2017-03-02 16:15:16 +0300

    fix annotations

--- Lazy/src/main/java/LazyFactory.java
+++ Lazy/src/main/java/LazyFactory.java
@@ -1,5 +1,7 @@
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
+
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Supplier;
