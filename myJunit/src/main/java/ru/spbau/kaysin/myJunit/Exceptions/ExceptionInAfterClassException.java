d5c5262f63275eb090a7ffbb1dd570e0daa9cd26
Ilya Kaysin <demarkok@gmail.com>
2017-05-13 21:28:20 +0300

    add afterClass and beforeClass exceptions

---
+++ myJunit/src/main/java/ru/spbau/kaysin/myJunit/Exceptions/ExceptionInAfterClassException.java
@@ -0,0 +1,11 @@
+package ru.spbau.kaysin.myJunit.Exceptions;
+
+/**
+ * Created by demarkok on 13-May-17.
+ */
+public class ExceptionInAfterClassException extends Exception {
+
+    public ExceptionInAfterClassException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ myJunit/src/main/java/ru/spbau/kaysin/myJunit/Exceptions/ExceptionInBeforeClassException.java
@@ -0,0 +1,11 @@
+package ru.spbau.kaysin.myJunit.Exceptions;
+
+/**
+ * Created by demarkok on 13-May-17.
+ */
+public class ExceptionInBeforeClassException extends Exception {
+
+    public ExceptionInBeforeClassException(Throwable cause) {
+        super(cause);
+    }
+}
